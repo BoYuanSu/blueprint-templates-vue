c4677a52e6d18e56ad51f002b5b0f0a4c2a2b61d
Paul Su <paul.su@ohbottech.com>
2021-10-05 16:04:31 +0800

    add vue3 sfc template (script setup)

---
+++ vue3-component-template-no-folder/__pascalCase_name__.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="{{kebabCase name}}">
+    {{pascalCase name}}
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.{{kebabCase name}} {
+}
+</style>
